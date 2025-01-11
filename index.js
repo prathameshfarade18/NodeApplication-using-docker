5cd08be73ee463ee96e8f76e9d026bb475d22428
prathameshfarade <prathameshfarade2000@gmail.com>
2025-01-11 14:54:58 +0530

    index file commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello Bhai log" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Node App ');
+ res.send(' change the line ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
