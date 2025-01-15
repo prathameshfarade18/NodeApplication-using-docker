d55b4a96797c95c0895863a391f6a9c1b39165b4
prathameshfarade <prathameshfarade2000@gmail.com>
2025-01-15 23:28:40 +0530

    index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
  
-var app = express();//Respond with "hello Bhai log" for requests that hit our root "/"
+var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('  Jenkins ');
+ res.send('hello MH');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
