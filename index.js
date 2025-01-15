df2ce6345ac736405e724134cf3797356588fb2c
prathameshfarade <prathameshfarade2000@gmail.com>
2025-01-15 23:01:49 +0530

    index commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello Bhai log" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send(' wellcome Jenkins ');
+ res.send(' well Jenkins ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
